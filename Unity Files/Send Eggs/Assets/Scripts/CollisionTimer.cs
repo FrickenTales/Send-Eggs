1c43305cc32a32ed1ab0d3bee42ba44ee361ada6
FrickenTales <taylormudgway@gmail.com>
2018-09-20 15:44:10 +1200

    Yolk Update

---
+++ Unity Files/Send Eggs/Assets/Scripts/CollisionTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTimer : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    public ParticleSystem yolk;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("collision registered");
+        StartCoroutine(Die());
+    }
+
+    public IEnumerator Die()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Instantiate(yolk, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+
+}
