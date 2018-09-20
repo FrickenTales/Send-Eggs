bc2090e5441c7b1097ba6745c5521328d4fce459
FrickenTales <taylormudgway@gmail.com>
2018-09-20 17:06:03 +1200

    Trap Level

---
+++ Unity Files/Send Eggs/Assets/Scripts/LeverTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrap : MonoBehaviour {
+
+    public GameObject player;
+    public PlayerController cntrl;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player_Rolling");
+        cntrl = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            cntrl.isDead = true;
+        }
+    }
+}
