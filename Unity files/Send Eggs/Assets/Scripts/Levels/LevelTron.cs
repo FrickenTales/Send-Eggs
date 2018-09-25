8a3b7f151df434577c00ea0c28ae105379233600
Cakeapo <jmannas@live.com>
2018-09-25 19:08:49 +1200

    Working on new levels
    Golf
    Tron
    Hat

---
+++ Unity files/Send Eggs/Assets/Scripts/Levels/LevelTron.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTron : MonoBehaviour {
+
+    private GameObject shell;
+    private Animator cartonanim;
+    private PlayerControllerTron player;
+    private WinObjective pan;
+    private ButtonScript panButton;
+    private Transform playerSpawn;
+    private LeverScript lever;
+    private GM gm;
+
+    private TrailRendererWith2DCollider trail;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        trail = GameObject.Find("TrailHost").GetComponent<TrailRendererWith2DCollider>();
+        shell = Resources.Load("BrokenEgg") as GameObject;
+        cartonanim = GameObject.FindGameObjectWithTag("Carton").GetComponent<Animator>();
+        gm = GameObject.Find("GameManager").GetComponent<GM>();
+
+        //player base stats
+        player = transform.GetChild(0).GetComponent<PlayerControllerTron>();
+        player.maxSpeed = 9;
+        player.jumpForce = 11;
+        player.canDoubleJump = false;
+
+        //pan
+        pan = GameObject.FindGameObjectWithTag("Objective").GetComponent<WinObjective>();
+        pan.ready = false;
+
+        //pan button
+        panButton = GameObject.FindGameObjectWithTag("PanButton").GetComponent<ButtonScript>();
+        panButton.isOn = false;
+
+        //bridge lever
+        lever = GameObject.FindGameObjectWithTag("Lever").GetComponent<LeverScript>();
+        lever.toggles = false;
+        lever.isOn = false;
+
+        //spawn point
+        playerSpawn = GameObject.Find("SpawnPoint").transform;
+
+        SpawnPlayer();
+	}
+
+    void KillPlayer()
+    {
+        trail.lifeTime = 0f;
+        Instantiate(shell, player.body.transform.position, player.body.transform.rotation);
+        gm.deathCount++;
+        Start();
+        //SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        player.transform.position = playerSpawn.position;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        cartonanim.SetTrigger("SpawnEgg");
+        player.anim.SetTrigger("Spawn");
+        Invoke("NewTrail", 0.1f);
+    }
+
+    void NewTrail()
+    {
+        trail.lifeTime = 60f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player.isDead)
+        {
+            player.isDead = false;
+            KillPlayer();
+        }
+	}
+}
---
+++ Unity files/Send Eggs/Assets/Scripts/PlayerControllerGolf.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerGolf : MonoBehaviour {
+
+    public bool walks;
+
+    public float maxSpeed;
+    public float jumpForce;
+    public bool canDoubleJump;
+    public float maxFallSpeed;
+    public float move;
+    private bool willDie = false;
+
+    public bool isDead = false;
+
+    private bool facingRight;
+    public Rigidbody2D rb2d;
+    public GameObject body;
+    public float facing;
+    public Animator anim;
+    public float animSpeed;
+
+    public bool grounded;
+    public Transform groundCheck;
+    private Vector2 groundCap = new Vector2 (0.8f,0.35f);
+    //private float groundRadius = 0.15f;
+    public LayerMask whatisGround;
+
+    private bool doubleJump = false;
+
+    private GM gm;
+
+    public GameObject club;
+    private Vector3 mousePos;
+    private Vector3 mouseStartPos;
+    private Vector3 playerStartPos;
+    private Vector3 mouseMove;
+    private Camera cam;
+    public float hitForce;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Cursor.visible = false;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        gm = GameObject.Find("GameManager").GetComponent<GM>();
+
+        rb2d = GetComponent<Rigidbody2D>();
+        body = transform.GetChild(0).GetChild(0).gameObject;
+        anim = transform.GetChild(0).GetComponent<Animator>();
+        groundCheck = transform.GetChild(1).transform;
+
+        Flip();
+
+        club = GameObject.Find("Club");
+        mouseStartPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+    }
+	
+
+	void FixedUpdate ()
+    {
+        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatisGround);
+        //grounded = Physics2D.OverlapCapsule(groundCheck.position, groundCap, CapsuleDirection2D.Horizontal, 90, whatisGround);
+        grounded = Physics2D.OverlapBox(groundCheck.position, groundCap, 0, whatisGround);
+
+        if (rb2d.velocity.y < maxFallSpeed)
+            willDie = true;
+
+        if (grounded && willDie)
+        {
+            isDead = true;
+            willDie = false;
+        }
+
+        if (grounded && canDoubleJump)
+            doubleJump = false;
+
+        /*
+        if (walks)
+        {
+            if (!gm.holdPlayer)
+            {
+                move = Input.GetAxis("Horizontal");
+
+                rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+            }
+
+            if (move > 0 && !facingRight)
+                Flip();
+            else if (move < 0 && facingRight)
+                Flip();
+
+            if (Mathf.Abs(rb2d.velocity.x) < 0.5f)
+                anim.SetBool("IsIdle", true);
+            else
+                anim.SetBool("IsIdle", false);
+        }
+        else
+        {
+            if (!gm.holdPlayer)
+            {
+                if (rb2d.velocity.x == 0)
+                {
+                    move = 0;
+                }
+
+                move = Mathf.Lerp(move, Input.GetAxis("Horizontal"), Time.deltaTime * 2.3f);
+            }
+
+            rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+            if (grounded)
+            {
+                animSpeed = rb2d.velocity.x / 4.5f;
+            }
+            else
+            {
+                animSpeed = Mathf.Lerp(animSpeed, rb2d.velocity.x / 3, Time.deltaTime * .7f);
+            }
+
+            anim.SetFloat("RollSpeed", animSpeed);
+
+        }
+        */
+    }
+
+    void Update()
+    {
+        animSpeed = rb2d.velocity.x / 3;
+        anim.SetFloat("RollSpeed", animSpeed);
+
+        if (!canDoubleJump)
+        {
+            doubleJump = true;
+        }
+
+        if((grounded || !doubleJump) && Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (grounded)
+            {
+                club.SetActive(true);
+
+                club.transform.up = body.transform.position - club.transform.position;
+
+                mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+                mouseMove = mousePos - mouseStartPos;
+                club.transform.position = body.transform.position + mouseMove;
+
+                hitForce = Vector2.Distance(club.transform.position, body.transform.position) * 3;
+            }
+        }
+        else
+        {
+            rb2d.AddForce(club.transform.up * hitForce, ForceMode2D.Impulse);
+            club.transform.position = body.transform.position;
+            hitForce = 0;
+            mouseStartPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            mouseMove = Vector3.zero;
+            club.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            isDead = true;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        if (facingRight)
+            facing = 0;
+        else
+            facing = 180;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, facing, transform.rotation.z);
+    }
+}
