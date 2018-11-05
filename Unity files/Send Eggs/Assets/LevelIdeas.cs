9026f1a95aa81134bfdbf3c08656908d004cdc93
Cakeapo <jmannas@live.com>
2018-11-06 12:51:37 +1300

    analyticss

--- Unity files/Send Eggs/Assets/LevelIdeas.cs
+++ Unity files/Send Eggs/Assets/LevelIdeas.cs
@@ -4,11 +4,11 @@
  ** Default Level
  ** Inverted controls
  ** Egg with legs
- * Scared pan - pan keeps running away from the player
+ ** Scared pan - pan keeps running away from the player
  ** Egg carton - play as the carton, eggs fall out when you jump
  ** Click buttons - buttons/levers have to actived with the mouse;
  * First or Third person Egg
- * Egg golf - Fling egg with mouse in golf game style
+ ** Egg golf - Fling egg with mouse in golf game style
  ** VVVVVV style - Jumping inverts gravity
  * Left 4 Egg - zombie eggs roam the level that the player can shoot
  ** Invisible level - no level, only colliders
@@ -19,7 +19,7 @@
  * Avoid the ghost - after winning and respawning, the ghost of your last run repeats the level too, the ghost kills you if you touch it
  ** Bouncy egg - 100% bounce + fast jump
  ** Move the level - controls now move the level around instead of the egg
- * lever trap - lever kills you
+ ** lever trap - lever kills you
  * one way lever
  * Invisible walls - level is a maze
  ** Mouse egg - Egg is stuck to curser;
